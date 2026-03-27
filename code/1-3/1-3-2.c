6d823df39c7946bb17935494fe1c54bde553f368
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-03-27 09:38:49 +0900

    Add logger functions and task2 implementation

---
+++ code/1-3/1-3-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void logger_global(char* message) {
+    static int counter = 0;
+    printf("log: %2d: %s\n", counter++, message);
+}
+
+void task2() {
+    for(int i=0; i<4; i++) {
+        logger_static_inline("task2");
+        logger_global("task2");
+    }
+}
