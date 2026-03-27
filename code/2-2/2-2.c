0a54009ba72b151e0a1e308d884e882b60cbf95a
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-03-27 09:53:21 +0900

    Update 2-2.c

---
+++ code/2-2/2-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    const char* var0 = "-300";
+
+    int var1 = atoi(var0);
+    float var2 = atof(var0);
+    int var3 = -(var1);
+
+    printf("%d %f %d\n", var1, var2, var3);
+    return 0;
+}
