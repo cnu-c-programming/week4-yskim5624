dc6fe50970941225076c22e9f4af0b0e876f2474
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-04-02 22:11:56 +0900

    Update 4-3.c

---
+++ code/4-3/4-3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {3, 7, 1, 9, 4, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i=0; i<(n-1); i++) {
+        void* addr_0 = &(arr[i]);
+        void* addr_1 = &(arr[i+1]);
+
+        printf("address of arr[%d]: %p\n", i, addr_0);
+        printf("address of arr[%d+1]: %p\n", i, addr_1);
+
+        printf("&(arr[%d]) - &(arr[%d+1]) = %d\n", i, i+1, (unsigned long long)addr_1 - (unsigned long long)addr_0);
+        printf("\n");
+    }
+    return 0;
+}
