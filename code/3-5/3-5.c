7330ea37b67cb43cf172c2f06cb0b9a1fa3b43d4
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-04-02 22:47:07 +0900

    Update 3-5.c

---
+++ code/3-5/3-5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void my_print(char type, void* x) {
+    switch (type) {
+    case 'C':
+        printf("%c\n", *(char*)x);
+        break;
+    case 'D':
+        printf("%d\n", *(int*)x);
+        break;
+    case 'S':
+       printf("%s\n", (char*)x);
+        break;
+    }
+}
+
+int main() {
+    int a = 0x61;
+    char b = 'b';
+    char* c = "hello world";
+
+    my_print('C', &a);
+    my_print('D', &b);
+    my_print('S', c);
+    my_print('C', c);
+
+    return 0;
+}
